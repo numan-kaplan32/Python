7d46e09e681bdb4ff9f5933f432afec11ca51e55
Numan Kaplan <numankaplan32@gmail.com>
2025-10-13 17:24:06 +0300

    Yaş Hesap
    Kullanıcıdan alınan gün, ay, yıl bilgilerine göre yaş hesaplayan basit bir Python console programı.

---
+++ AgeCalculater.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 13 17:02:02 2025
+
+@author: numan
+"""
+import datetime
+
+dayUser = int(input("Doğum gününüz (gün): "))
+monthUser = int(input("Doğum gününüz (ay): "))
+yearUser = int(input("Doğum gününüz (yıl): "))
+
+userBirthday = datetime.date(yearUser, monthUser, dayUser)
+today = datetime.date.today()
+age = today.year - userBirthday.year
+if (today.month, today.day) < (userBirthday.month, userBirthday.day):
+    age -= 1
+
+print(f"Doğum tarihiniz: {userBirthday.strftime('%d.%m.%Y')}")
+print(f"Bugünün tarihi: {today.strftime('%d.%m.%Y')}")
+print(f"Yaşınız: {age}")
